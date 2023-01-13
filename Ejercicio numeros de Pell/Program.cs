01b017b1c47835303c80f99ce5f2e53f44dbce61
AlMonty11 <118449139+AlMonty11@users.noreply.github.com>
2023-01-13 21:50:10 +0000

    Ejercicio Numeros de Pell
    Incluida solo la carpeta del ejercicio. No la solución (prueba)

---
+++ Ejercicio numeros de Pell/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace Ejercicio_numeros_de_Pell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* la secuencia de números de Pell comienza con 0 y 1, 
+             * y luego cada número de Pell se obtiene de los dos anteriores:
+             * es la suma de dos veces el número de Pell anterior
+             * y del número de Pell previo a este.Los primeros términos de la secuencia son
+             0, 1, 2, 5, 12,*/
+            int start = 0;
+            int end = 15;
+            int[] pell= new int[end];
+
+            for (int i = 0; i < end; i++)
+            {
+                if (i < 2)
+                {
+                    pell[i] = start;
+                    start++;
+                    Console.Write(pell[1]+" | ");
+                }
+                else
+                {
+                    pell[i] = (pell[i - 2]) + (pell[i - 1] * 2);
+                    start++;
+                    Console.Write(pell[i] + " | ");
+                }
+               
+            }
+        }
+    }
+}
